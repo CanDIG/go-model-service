79a39cf78fe4d16aafb2c660bcaa7fe028bf04e5
katpavlova <katpavlova.ccm@gmail.com>
2020-05-05 18:05:28 -0400

    [WIP] Dockerize the v1 service

--- model-vs/api/generate_handlers.sh
+++ model-vs/api/generate_handlers.sh
@@ -2,25 +2,23 @@
 
 # TODO make smarter by only re-generating files if they've been modified (makefile?)
 
-apiDir=$GOPATH/src/github.com/CanDIG/go-model-service/model-vs/api
-
 # Generate handler utilities for the following resources: Individual, Variant, Call
-genny -in $apiDir/generics/generic_resource_utilities.go -out $apiDir/restapi/utilities/resource_utilities.go -pkg utilities gen "Individual=Individual,Variant,Call"
+genny -in $API_PATH/generics/generic_resource_utilities.go -out $API_PATH/restapi/utilities/resource_utilities.go -pkg utilities gen "Individual=Individual,Variant,Call"
 
 # Generate POST handlers for the following resources: Individual, Variant, Call
-genny -in $apiDir/generics/generic_post.go -out $apiDir/restapi/handlers/post.go -pkg handlers gen "Individual=Individual,Variant,Call"
+genny -in $API_PATH/generics/generic_post.go -out $API_PATH/restapi/handlers/post.go -pkg handlers gen "Individual=Individual,Variant,Call"
 
 # Generate GET (one) handler for the following resources: Individual, Variant, Call
-genny -in $apiDir/generics/generic_get_one.go -out $apiDir/restapi/handlers/get_one.go -pkg handlers gen "Individual=Individual,Variant,Call"
+genny -in $API_PATH/generics/generic_get_one.go -out $API_PATH/restapi/handlers/get_one.go -pkg handlers gen "Individual=Individual,Variant,Call"
 
 # Generate GET (many) handlers for the following resources: Individual, Variant, Call
-genny -in $apiDir/generics/generic_get_many.go -out $apiDir/restapi/handlers/get_many.go -pkg handlers gen "Individual=Individual,Variant,Call"
+genny -in $API_PATH/generics/generic_get_many.go -out $API_PATH/restapi/handlers/get_many.go -pkg handlers gen "Individual=Individual,Variant,Call"
 
 # Generate GET (by another resource) handlers for the following resources: {Individuals by Variant with junction in Call}, {Variants by Individual with junction in Call}
 # There are presently issues with the genny tool that prevent this step from working properly.
 # Please see github.com/CanDIG/genny/issues to help resolve some of these issues.
-# genny -in $apiDir/generics/generic_get_by.go -pkg handlers gen "Individual=Individual Variant=Variant Call=Call" > $apiDir/restapi/handlers/get_by.go
-# genny -in $apiDir/generics/generic_get_by.go -pkg gen "Individual=Variant Variant=Individual Call=Call" >> $apiDir/restapi/handlers/get_by.go
+# genny -in $API_PATH/generics/generic_get_by.go -pkg handlers gen "Individual=Individual Variant=Variant Call=Call" > $API_PATH/restapi/handlers/get_by.go
+# genny -in $API_PATH/generics/generic_get_by.go -pkg gen "Individual=Variant Variant=Individual Call=Call" >> $API_PATH/restapi/handlers/get_by.go
 
 # Generate transformers for the following resources: Individual, Variant, Call
-genny -in $apiDir/generics/generic_transformations.go -out $apiDir/restapi/handlers/transformations.go -pkg handlers gen "Individual=Individual,Variant,Call"
+genny -in $API_PATH/generics/generic_transformations.go -out $API_PATH/restapi/handlers/transformations.go -pkg handlers gen "Individual=Individual,Variant,Call"
