1b6a09a8239ff1d8cbc1b89859e32de1e2b39ace
katpavlova <katpavlova.ccm@gmail.com>
2020-05-05 18:05:28 -0400

    [WIP] Dockerize the v1 service

--- alt-install/install.sh
+++ alt-install/install.sh
@@ -4,12 +4,19 @@
 # for running the service.
 # It also creates a new development database to write into.
 
+# Set up working directory for this script
+export WORK_DIR=$GOPATH/src/github.com/CanDIG/go-model-service
+cd $WORK_DIR
+
 # Set up path to database configuration file. 
 # This database config file must be kept within one of the following paths, relative to
 # the file attempting to make the database connection:
 # "", "./config", "/config", "../", "../config", "../..", "../../config", "APP_PATH", "POP_PATH"
 # For more information: https://github.com/gobuffalo/pop/blob/master/config.go
-export POP_PATH=$GOPATH/src/github.com/CanDIG/go-model-service/database
+export POP_PATH=$WORK_DIR
+
+# Directory to API files, to be used by codegen
+export API_PATH=$WORK_DIR/model-vs/api
 
 # Use the dep tool to install all project import dependencies in a
 # new vendor directory.
@@ -30,17 +37,25 @@ soda create -c ./database.yml -e development
 soda migrate up -c ./database.yml -e development -p model-vs/data/migrations
 
 # Swagger generate the boilerplate code necessary for handling API requests
-# from the model-vs/api/swagger.yml template file
-cd model-vs/api
-swagger generate server -A variant-service swagger.yml # This will generate a server named variant-service, which is important for maintaining compatibility with the configure_variant_service.go middleware configuration file.
-cd ../..
+# from the model-vs/api/swagger.yml template file.
+# This will generate a server named variant-service. The name is important for 
+# maintaining compatibility with the configure_variant_service.go middleware 
+# configuration file.
+cd $API_PATH
+swagger generate server -A variant-service swagger.yml
+cd $WORK_DIR
 
 # Run a script to generate resource-specific request handlers for middleware,
 # from the generic handlers defined in the model-vs/api/generics package,
 # using the CanDIG-maintained CLI tool genny
-cd model-vs/api
-./generate_handlers.sh
-cd ../..
+$API_PATH/generate_handlers.sh
 
 # Now that all the necessary boilerplate code has been auto-generated, compile the server
-go build -tags sqlite -o ./main model-vs/api/cmd/variant-service-server/main.go
+go build -tags sqlite -o ./main $API_PATH/cmd/variant-service-server/main.go
+
+# Unset working directory since it is no longer needed
+cd $WORK_DIR
+unset WORK_DIR
+
+# Now you can run ./main to launch the variant service
+# ./main --port=3000
