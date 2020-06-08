5266ef31955d2faff4b6529f3035b5db383ee1a7
katpavlova <katpavlova.ccm@gmail.com>
2020-06-08 17:41:12 -0400

    [bugfix] More rigorous check of database readiness

--- migrate.sh
+++ migrate.sh
@@ -7,5 +7,7 @@
 # wait-for-it waits for the Postgres DB to be ready to accept connections.
 
 docker-compose exec gms-webapp sh -c \
-	"wait-for-it database:5432 --timeout=45 -- \
+	"until pg_isready --timeout=60 --username=$POSTGRES_USER --host=database; \
+	do sleep 1; \
+	done &&\
 	soda migrate up -c ./database.yml -e development -p model-vs/data/migrations"
